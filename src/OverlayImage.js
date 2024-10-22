7e792038014d15f8a57b59c248170f530077cdcd
subham.singh <subham.singh@zohocorp.com>
2024-10-22 18:03:13 +0530

    upload and download functionality

---
+++ src/OverlayImage.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState, useRef } from "react";
+import { CameraOutlined, DownloadOutlined } from "@ant-design/icons";
+import { Button, Upload, message } from "antd";
+
+const ImageOverlay = () => {
+  const frameImage = "/frames/frame1.png";
+
+  const [overlayImage, setOverlayImage] = useState(null);
+  const canvasRef = useRef(null);
+
+  const downloadImage = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    canvas.toBlob((blob) => {
+      if (blob) {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "awareness_frame.png";
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+        URL.revokeObjectURL(link.href);
+      }
+    }, "image/png");
+  };
+
+  const handleOverlayImageUpload = (info) => {
+    const file = info.fileList[0].originFileObj;
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setOverlayImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleOverlay = () => {
+    if (!overlayImage || !frameImage) {
+      message.error("Please upload both base and overlay images.");
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const baseImg = new Image();
+    const overlayImg = new Image();
+
+    baseImg.src = frameImage;
+    overlayImg.src = overlayImage;
+
+    baseImg.onload = () => {
+      canvas.width = baseImg.width;
+      canvas.height = baseImg.height;
+
+      ctx.drawImage(baseImg, 0, 0);
+
+      overlayImg.onload = () => {
+        const overlayWidth = 730;
+        const overlayHeight = 730;
+        const xPosition = 2048;
+        const yPosition = 1070;
+
+        ctx.drawImage(
+          overlayImg,
+          xPosition,
+          yPosition,
+          overlayWidth,
+          overlayHeight
+        );
+      };
+
+      overlayImg.onerror = () => {
+        message.error("Failed to load the overlay image.");
+      };
+    };
+
+    baseImg.onerror = () => {
+      message.error("Failed to load the base image.");
+    };
+  };
+
+  useEffect(() => {
+    if (overlayImage) {
+      handleOverlay();
+    }
+  }, [overlayImage]);
+
+  return (
+    <div>
+      <div className="header">
+        <div>
+          <h2>Awareness Frame</h2>
+        </div>
+        {overlayImage ? (
+          <div>
+            <canvas ref={canvasRef} className="canvas-frame"></canvas>
+          </div>
+        ) : (
+          <div>
+            <img src={frameImage} alt="Base Preview" className="canvas-frame" />
+          </div>
+        )}
+      </div>
+
+      <div className="actions">
+        <Upload
+          maxCount={1}
+          showUploadList={false}
+          beforeUpload={() => false}
+          onChange={handleOverlayImageUpload}
+        >
+          <Button
+            icon={<CameraOutlined />}
+            type="dashed"
+            className="upload-btn"
+          >
+            Choose a Photo
+          </Button>
+        </Upload>
+
+        {frameImage && overlayImage && (
+          <Button
+            icon={<DownloadOutlined />}
+            onClick={downloadImage}
+            className="download-btn"
+          >
+            Download Image
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ImageOverlay;
